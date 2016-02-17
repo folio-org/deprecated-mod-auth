4e6aafeaf8f8b48bda1225b9f002a51c8f1a0c20
Kurt Nordstrom <kurt.e.nordstrom@gmail.com>
2016-02-16 22:19:40 -0500

    Set accept handler to pass along data

--- src/main/java/okapi-modules/MainVerticle.java
+++ src/main/java/okapi-modules/MainVerticle.java
@@ -10,19 +10,12 @@ import io.vertx.core.json.DecodeException;
 import io.vertx.ext.web.handler.BodyHandler;
 import io.vertx.ext.auth.jwt.JWTAuth;
 import io.vertx.ext.auth.jwt.JWTOptions;
-
-import io.vertx.core.impl.VertxInternal;
-import java.io.FileInputStream;
-import java.security.KeyStore;
-import java.security.Key;
-import javax.crypto.Mac;
-import io.vertx.ext.auth.jwt.impl.JWT;
-
 public class MainVerticle extends AbstractVerticle {
 
-  private final String signingSecret = "secret";
+  private final String signingSecret = "all_zombies_must_dance";
   private AuthUtil authUtil = new AuthUtil();
   private JWTAuth jwtAuth = null;
+  private Long expires = 60L;
   @Override
   public void start(Future<Void> fut) {
   
@@ -37,20 +30,40 @@ public class MainVerticle extends AbstractVerticle {
 
     Router router = Router.router(vertx);
 
-    //router.post("/login").handler(this::handleLogin);
-    //router.post("/validate").handler(this::handleValidation);
-    router.get("/").handler(this::handleIndex);
     router.post("/token").handler(BodyHandler.create()); //Allow us to read the POST data
     router.post("/token").handler(this::handleCreateToken);
+    router.get("/*").handler(this::handleAuth);
+    router.get("/test").handler(this::handleTest);
     
     HttpServer server = vertx.createHttpServer();
     server.requestHandler(router::accept).listen(8081);  
   }
 
-  private void handleIndex(RoutingContext ctx) {
-    ctx.response().setStatusCode(200);
-    ctx.response().putHeader("content-type", "text/plain");
-    ctx.response().end("<h1>Hello!</h1>");
+  private void handleAuth(RoutingContext ctx) {
+    String authToken = ctx.request().headers().get("X-Okapi-Authorization");
+    JsonObject authInfo = new JsonObject().put("jwt", authToken);
+    jwtAuth.authenticate(authInfo, result -> {
+      if(!result.succeeded()) {
+        System.out.println("Did not succeed");
+        ctx.response().setStatusCode(400);
+        ctx.response().end("Denied");
+      
+      } else {
+        //Assuming that all is well, switch to chunked and return the content
+        ctx.response().setChunked(true);
+        ctx.response().setStatusCode(200);
+        //Assign a handler that simply writes back the data
+        ctx.request().handler( data -> {
+          System.out.println("Writing data to response");
+          ctx.response().write(data);
+        });
+      //Assign an end handler that closes the request
+        ctx.request().endHandler( data -> {
+          System.out.println("Closing response");
+          ctx.response().end();
+        });
+      }
+    });
   }
 
   
@@ -76,10 +89,16 @@ public class MainVerticle extends AbstractVerticle {
     ctx.response().setStatusCode(200);
     String token = jwtAuth.generateToken(
         new JsonObject().put("username", json.getString("username")),
-        new JWTOptions()
+        new JWTOptions().setExpiresInSeconds(expires)
     );
     ctx.response().putHeader("Content-Type", "text/plain");
     ctx.response().end(token);
+  }
+
+  private void handleTest(RoutingContext ctx) {
+    ctx.response().setStatusCode(200);
+    ctx.response().putHeader("Content-Type", "text/plain");
+    ctx.response().end("You have passed the test.");
   }
 
 /*
